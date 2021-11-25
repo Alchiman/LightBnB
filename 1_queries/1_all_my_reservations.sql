a88adb63937721e68b2243b16a878b278b942948
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-24 20:26:49 -0800

    cleaned up some comments

--- 1_queries/1_all_my_reservations.sql
+++ 1_queries/1_all_my_reservations.sql
@@ -1,11 +1,3 @@
--- Select all columns from the reservations table, all columns from the properties table, and the average rating of the property.
--- The reservations will be for a single user, so use 1 for the user_id.
--- Order the results from the earliest start_date to the most recent start_date.
--- These will end up being filtered by either "Upcoming Reservations" or "Past Reservations", so only get reservations where the end_date is in the past.
--- Use now()::date to get today's date.
--- Limit the results to 10.
-
-
 SELECT properties.*, reservations.*, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
