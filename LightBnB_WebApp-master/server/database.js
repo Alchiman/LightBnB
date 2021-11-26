721c1738df3873e470c5e6f3fe64ba5a29b77db3
Alireza Chaychi <alireza.chaychi@gmail.com>
2021-11-25 21:34:24 -0800

    added search filters to getAllpropeties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -97,9 +97,60 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+    `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+
+    queryParams.length === 1
+      ? (queryString += `AND WHERE properties.owner_id = $${queryParams.length}`)
+      : (queryString += `WHERE properties.owner_id = $${queryParams.length}`);
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(
+      options.minimum_price_per_night * 100,
+      options.maximum_price_per_night * 100
+    );
+    queryParams.length !== 2
+      ? (queryString += `AND cost_per_night >= $${queryParams.length -
+          1} AND cost_per_night <= $${queryParams.length} `)
+      : (queryString += `WHERE cost_per_night >= $${queryParams.length -
+          1} AND cost_per_night <= $${queryParams.length} `);
+  }
+
+  queryString += ` GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
   return pool
-    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
-    .then((results) => results.rows)
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
